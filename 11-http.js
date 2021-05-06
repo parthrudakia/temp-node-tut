c7c43aad2130f133ba0413eec3d22b7d89b2fa7f
Parth Rudakia <parthrudakia@Parths-MacBook-Pro.local>
2021-05-06 22:51:46 +0530

    removed Lodash dependency

--- 11-http.js
+++ 11-http.js
@@ -1,11 +1,12 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-
-    if(req.url === "/") {
-        res.end('home')
-    }
-    res.end(`<h1>Oops!!!</h1>`)
+    console.log("request event")
+    // if(req.url === "/") {
+    //     res.end('home')
+    // }
+    // res.end(`<h1>Oops!!!</h1>`)
+    res.end("hello")
 })
 
 server.listen(3000)
